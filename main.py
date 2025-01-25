fd168f60c7aa60883209c4a69dd2e71a623febd1
Luna Wang <lunawang257@gmail.com>
2025-01-24 21:22:25 -0800

    removed my copious use of global variables + bad style

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import csv
 import random
-import datetime
+from datetime import datetime
 
-max_drawdown = 0
-max_money = 0
+# Constants
 START_MONEY = 1
 TAX_RATE = 0.5
+DEFAULT_PERCENTILES = [25, 50, 75]
+DEFAULT_SAMPLE_TIMES = 5
 
-class all_years_info:
-    def __init__(self, end_money, max_drawdown, gain_per, annualized):
+class AllYearsInfo:
+    """Class to store and compare investment performance metrics."""
+    def __init__(self, end_money: float, max_drawdown: float, gain_per: float, annualized: float):
         self.end_money = end_money
         self.max_drawdown = max_drawdown
         self.gain_per = gain_per
@@ -20,7 +22,64 @@ class all_years_info:
     def __eq__(self, other):
         return self.end_money == other.end_money
 
-def read_annual_data(filename):
+class Investment:
+    """Class to store and calculate investment results."""
+    def __init__(self, start_money: float = 1, tax_rate: float = 0.5):
+        self.start_money = start_money
+        self.tax_rate = tax_rate
+        self.max_drawdown = 0
+        self.max_money = 0
+
+    def calc_one_year(self, old_money: float, data: dict, protection: float, cap: float) -> float:
+        """Calculate investment results for one year with optional protection and cap.
+    
+        Args:
+            old_money: Starting amount of money
+            data: Dictionary containing price gain and yield data
+            protection: Downside protection percentage
+            cap: Maximum gain cap (-1 for no cap)
+        
+        Returns:
+            float: New amount of money after calculations
+        """
+        buyhold_mon = old_money
+        buyhold_mon += buyhold_mon * data['pricegain']
+        dividend = buyhold_mon * data['yield']
+        buyhold_mon += dividend - dividend * self.tax_rate
+        
+        if buyhold_mon < old_money:
+            lost = 1 - buyhold_mon / old_money
+            new_money = old_money - old_money * max(lost - protection, 0)
+        elif cap != -1:
+            gained = buyhold_mon / old_money - 1
+            gained = min(gained, cap)
+            new_money = old_money + old_money * gained
+        else:
+            new_money = buyhold_mon
+
+        if new_money > self.max_money:
+            self.max_money = new_money
+        drawdown = 1 - new_money / self.max_money
+
+        if drawdown > self.max_drawdown:
+            self.max_drawdown = drawdown
+
+        return new_money
+    
+    def calc_all_years(self, data: list, protection: float, cap: float) -> AllYearsInfo:
+        self.max_drawdown = 0
+        self.max_money = 0
+
+        cur_money = self.start_money
+        for year_data in data:
+            cur_money = self.calc_one_year(cur_money, year_data, protection, cap)
+
+        gain = cur_money / self.start_money - 1
+        annualized_gain = pow(gain, 1 / (len(data) - 1)) - 1
+
+        return AllYearsInfo(cur_money, self.max_drawdown, gain, annualized_gain)
+
+def read_annual_data(filename: str) -> list:
     data = []
 
     with open(filename, mode='r') as file:
@@ -40,7 +99,7 @@ def read_annual_data(filename):
     return data
 
 # date, adjusted close (adj_close)
-def read_monthly_data(filename):
+def read_monthly_data(filename: str) -> list:
     data = []
 
     with open(filename, mode='r') as file:
@@ -49,14 +108,14 @@ def read_monthly_data(filename):
         for line_dict in csvfile:
             line_dict = dict(line_dict)
 
-            line_dict['date'] = datetime.strptime(line_dict['date'], "%Y-%m-%d")
+            line_dict['date'] = datetime.strptime(line_dict['date'], '%Y-%m-%d')
 
             if line_dict['date'].month == 1:
                 data.append( {'date': line_dict['date'], 'adj_close': float(line_dict['adj_close'])} )
     
     return data
 
-def calc_multiverse(data, want=[25, 50, 75], sample_times=5):
+def calc_multiverse(data: list, want: list = [25, 50, 75], sample_times: int = 5) -> list:
     end_moneys = []
     data = data.copy()
     for i in range(sample_times):
@@ -74,61 +133,17 @@ def calc_multiverse(data, want=[25, 50, 75], sample_times=5):
     out = []
 
     for per in want:
-        nth = int(per / 100 * sample) # TODO 0 or 1 indexing??
+        nth = int(per / 100 * sample_times)
         out.append(end_moneys[nth])
     
     return out
 
-def calc_one_year(old_money, data: dict, protection, cap):
-    global max_drawdown
-    global max_money
-
-    buyhold_mon = old_money
-    buyhold_mon += buyhold_mon * data['pricegain']
-    dividend = buyhold_mon * data['yield']
-    buyhold_mon += dividend - dividend * TAX_RATE
-    
-    if buyhold_mon < old_money:
-        # might need protection
-        lost = 1 - buyhold_mon / old_money
-        new_money = old_money - old_money * max(lost - protection, 0)
-    elif cap != -1:
-        # might hit cap
-        gained = buyhold_mon / old_money - 1
-        gained = min(gained, cap)
-        new_money = old_money + old_money * gained
-    else:
-        new_money = buyhold_mon
-
-    if new_money > max_money:
-        max_money = new_money
-    drawdown = 1 - new_money / max_money
-
-    if drawdown > max_drawdown:
-        max_drawdown = drawdown
-
-    return new_money
-
-# cap = -1 means no cap
-def calc_all_years(data, protection, cap):
-    global max_drawdown
-    global max_money
-    max_drawdown = 0
-    max_money = 0
-
-    cur_money = START_MONEY
-    for year_data in data:
-        cur_money = calc_one_year(cur_money, year_data, protection, cap)
-
-    gain = cur_money / START_MONEY - 1
-    annualized_gain = pow(gain, 1 / (len(data) - 1)) - 1
-
-    return all_years_info(cur_money, max_drawdown, gain, annualized_gain)
-
 def calc_and_print(data, protection, cap):
     print_cap = 'no' if cap == -1 else f'{cap * 100:.3f}%'
     print(f'\n*** Results for {protection * 100:.3f}% protection, {print_cap} cap ***')
-    info = calc_all_years(data, protection, cap)
+
+    investment = Investment(START_MONEY, TAX_RATE)
+    info = investment.calc_all_years(data, protection, cap)
     
     #print(f'Starting money: ${START_MONEY}\nEnding money: ${end_money:.2f}')
     print(f'Max drawdown: {info.max_drawdown * 100:.3f}%\nAnnualized gain: {info.annualized * 100:.3f}%')
